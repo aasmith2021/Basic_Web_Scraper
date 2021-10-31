fd9aac800ec2ccfb5c628c376addf0dee2bb8613
Aaron Smith <aaron.j.smith2019@gmail.com>
2021-10-31 17:21:04 -0400

    Add comment to dataSplitters.js

--- dataSplitters.js
+++ dataSplitters.js
@@ -1,6 +1,6 @@
-// Each array in <dataSplitters> contains two strings: the first
-// is the html before the text you want to grab, and the second
-// is the html after. They are the "bookends" for the text to keep.
+// Each array in <dataSplitters> contains two strings: the first is the html before
+// the text you want to grab, and the second is the html after. They are the "bookends"
+// for the text to keep. Each dataSplitter will be used on every page of the urls from the urls.js file
 const dataSplitters = [
     ['<h2>', ' <i'],
     ['"latin_name">(', ')'],
